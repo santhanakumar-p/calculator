5b050a575e847007465b0ac4aa5c722584f1c664
Santhanakumar P <santhanakumar99@outlook.com>
2023-07-25 11:53:48 +0530

    sslc_marks

---
+++ view/login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+
+<body>
+    <div>
+        <center>
+            <div style="background-color:aqua; height:auto; width: 500px;">
+                <form action="">
+                    <div>
+                        <label for="registrationNo">Registration No : </label>
+                        <input type="number" name="registration_no" id="registrationNo" required><br><br>
+                    </div>
+                    <div>
+                        <label for="dob">Enter Date of Birth(dd/mm/yyyy) : </label>
+                        <input type="password" name="dob" id="dob" placeholder="dd/mm/yyyy" required><br><br>
+                    </div>
+
+                    <div>
+                        <button type="submit">Get Marks</button>
+                        <button type="reset">Clear</button>
+                    </div>
+            </div>
+        </center>
+        </form>
+    </div>
+</body>
+
+</html>
